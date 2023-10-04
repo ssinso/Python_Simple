64333cf7b594f6facbedf39a7ef78a8efdeee3ed
ssinso <ijijin6@gmail.com>
2023-10-04 17:29:12 +0900

    2023.09.20

---
+++ lecture/python_05_for.py
@@ -0,0 +1,27 @@
+# 제어문
+# 1.조건문(if)
+# 2.반복문(for, while)
+
+# 반복문(Loop)
+# - 반복적인 작업을 가능하게 해주는 도구
+# - list, str, tuple 등 컬렉션 아비의 아이템을 하나씩
+#   순회하면서 사용 가능(for)
+# - 특정 조건을 만족하는 경우(while)
+
+# 반복횟수 0 => for
+# 반복횟수 X => while
+
+# for문 기본문법(list 활용)
+for num in [1, 2, 3]:
+    print(num)
+
+# range() 함수
+# - range(시작, 끝, 증감)
+# - default
+
+
+
+for num in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    print("2x")
+    print(num) print("=") print(num*2)
+
---
+++ practice/practice_01_for.py
@@ -0,0 +1,18 @@
+# 문제1) 사용자가 입력한 단수를 출력하는 코드
+# dan = int(input("단수: "))
+# for ab in range(1, 10):
+#    print(f"{dan}x{ab}={dan*ab}")
+
+# # 문제2) 2단부터 9단까지 출력(중첩 for문)
+# for i in range(2, 10):
+#     for j in range(1, 10):
+#         print(f"{i}x{j}={i*j}")
+
+# 문제3) list a 의 평균값을 계산하세요
+a = [1, 2, 3, 4, 5, 99, 87, 54, 2, 5, 4]
+# a길이 => len(a)
+total = 0
+for num in a:
+    total+= num
+result = total / len(a)
+print(round(result, 2)) # 평균값
